61fdda3ac185b03d1d59373ed4979cf304fd625f
lawden <lawtaden@gmail.com>
2024-07-08 20:14:39 +0200

    basic async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,18 +5,8 @@ This module contains the definition of the asynchronous coroutine wait_random.
 
 import asyncio
 import random
-from typing import Union
 
-async def wait_random(max_delay: int = 10) -> Union[float, int]:
-    """
-    Waits for a random delay between 0 and max_delay seconds and returns the delay.
-
-    Args:
-        max_delay (int): Maximum delay in seconds (default is 10).
-
-    Returns:
-        float: Random delay between 0 and max_delay.
-    """
+async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
