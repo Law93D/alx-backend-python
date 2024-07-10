b6118cfa5801662191fb1dce5203982d7b99e29f
lawden <lawtaden@gmail.com>
2024-07-10 16:52:44 +0200

    async comp

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,7 @@ random numbers asynchronously.
 """
 
 from typing import List
-from 0_async_generator import async_generator
+from async_generator import async_generator
 
 async def async_comprehension() -> List[float]:
     """
