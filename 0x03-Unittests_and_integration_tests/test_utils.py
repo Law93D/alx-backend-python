64f82351b1c67a4cf7adac5a4bfd52a72a230d38
lawden <lawtaden@gmail.com>
2024-07-27 09:34:12 +0200

    test utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -48,5 +48,26 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(result, test_payload)
 
 
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with (patch.object(TestClass, 'a_method', return_value=42)
+                as mock_method):
+            test_instance = TestClass()
+            # Call a_property twice and check the result
+            self.assertEqual(test_instance.a_property, 42)
+            self.assertEqual(test_instance.a_property, 42)
+            # Assert that a_method was called only once
+            mock_method.assert_called_once()
+
+
 if __name__ == "__main__":
     unittest.main()
