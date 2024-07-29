a91900aa97eab4b6aa4742bd7ead08c22f621118
lawden <lawtaden@gmail.com>
2024-07-29 23:27:07 +0200

    test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,5 +36,44 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, "https://api.github.com/orgs/google/repos")
 
 
+class TestGithubOrgClient(unittest.TestCase):
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        mock_org.return_value = (
+                {"repos_url": "https://api.github.com/orgs/google/repos"})
+        # Create an instance of GithubOrgClient
+        client = GithubOrgClient("google")
+        result = client._public_repos_url
+        self.assertEqual(
+                (result, "https://api.github.com/orgs/google/repos")
+            )
+
+    @patch(
+        'client.get_json',
+        return_value=[{"name": "repo1"}, {"name": "repo2"}]
+    )
+    @patch(
+        'client.GithubOrgClient._public_repos_url',
+        new_callable=PropertyMock
+    )
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        # Set up the mocked URL
+        mock_public_repos_url.return_value = (
+            "https://api.github.com/orgs/google/repos"
+        )
+
+        # Create an instance of GithubOrgClient
+        client = GithubOrgClient("google")
+        result = client.public_repos()
+        self.assertEqual(result, ["repo1", "repo2"])
+
+        # Assert that the mocked property and get_json were called once
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with(
+                ("https://api.github.com/orgs/google/repos")
+            )
+
+
 if __name__ == "__main__":
     unittest.main()
