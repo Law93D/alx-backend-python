69ad8eddb8f98457fed7bdb5f572818ecbc90a7d
lawden <lawtaden@gmail.com>
2024-07-29 23:01:18 +0200

    test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import unittest
 from unittest import TestCase
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized, parameterized_class
@@ -13,13 +14,14 @@ class TestGithubOrgClient(TestCase):
         ("google",),
         ("abc",)
     ])
-    @patch('client.get_json')
+    @patch('client.get_json', return_value={'key': 'value'})
     def test_org(self, org_name, mock_get_json):
         client = GithubOrgClient(org_name)
-        client.org()
+        result = client.org()
         mock_get_json.assert_called_once_with(
                 f"https://api.github.com/orgs/{org_name}"
             )
+        self.assertEqual(result, {'key': 'value'})
 
 
 if __name__ == "__main__":
