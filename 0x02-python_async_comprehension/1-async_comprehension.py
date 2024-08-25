8fc4fc0462fdbe1d715b36a9c6e6c8faa6f8ec7e
Law93D <lawtaden@gmail.com>
2024-08-25 09:45:15 +0200

    async comp

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+"""Task 1's module"""
+from importlib import import_module as using
 import asyncio
 from typing import List
-from async_generator import async_generator
+
+
+async_generator = using('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """Collects 10 random numbers using async comprehending over async_generator."""
